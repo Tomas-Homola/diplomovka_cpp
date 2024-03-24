3699b01b124147aab5bc2359ead1498fcf3fd33d
Tomas-Homola <homola.tom6@gmail.com>
2024-03-25 00:40:23 +0100

    Nové veci
    - kód roztriedený do viacero funkcií
    - hotová redistribúcia bodov podľa požadovanej
      výpočtovej mriežky
    - paralelizácia tam, kde sa hodí
    - úprava názvu export súboru: pridaný dátum a čas
      vytvorenia

--- src/diplomovka_TH.cpp
+++ src/diplomovka_TH.cpp
@@ -9,66 +9,13 @@ DataHandler::DataHandler(QWidget* parent)
 	int nprocs = 6;
 	omp_set_num_threads(nprocs);
 
-	QString infoFile = "E:/_school/_diplomovka/c++/BielyKriz_area.kml_data.txt";
-
-	QFile inputFile = QFile(infoFile);
-	if (!inputFile.open(QIODevice::ReadOnly))
-	{
-		std::cout << "Info file failed to open\n";
-		exit(-1);
-	}
+	QString infoFile = "E:/_school/_diplomovka/c++/91E0_area.kml_data.txt";
 	
-	QTextStream inStream(&inputFile);
+	//std::vector<double> v = { 1.0,2.0,3.0,6.0,6.0,7.0,11.0,12.0,15.0,16.0,19.0,19.0,20.0,21.0,23.0,27.0,30.0,34.0,36.0,40.0 };
 
-	readInputFile(inStream);
+	performCalculation(infoFile);
 
-	inputFile.close();
-
-	std::cout << "width_n: " << m_areaInfo.width_n << "\nheight_n: " << m_areaInfo.height_n << "\n";
-	std::cout << "width: " << m_areaInfo.width << "\nheight: " << m_areaInfo.height << "\n";
-	std::cout << "(x,y): [" << m_areaInfo.xLeft << ", " << m_areaInfo.yLeft << "]\n";
-	std::cout << "laz files: " << m_areaInfo.lazFiles.size() << "\n";
-	
-	for (size_t i = 0; i < m_areaInfo.lazFiles.size(); i++)
-	{
-		std::cout << "file " << i << ": " << m_areaInfo.lazFiles[i] << "\n";
-	}
-
-	int nPixels = m_areaInfo.width_n * m_areaInfo.height_n;
-	m_meshPixels.resize(nPixels, Points());
-
-
-	if (!readLazFiles())
-	{
-		std::cout << "Failed to read laz files\n";
-		exit(-2);
-	}
-
-	normalizePoints();
-
-	//std::vector<double> a = { 1, 2, 3, 4 };
-	//std::vector<double> b = { 5, 6, 7 };
-	//
-	//a.insert(std::end(a), std::begin(b), std::end(b));
-	//b.clear();
-	//b.shrink_to_fit();
-
-	m_metrics.resize(1);
-	m_metrics[0] = new double[nPixels] {};
-
-	double maxZ = -1.0;
-	for (int i = 0; i < nPixels; i++)
-	{
-		m_metrics[0][i] = NAN;
-		if (m_meshPixels[i].zCoords.empty())
-			continue;
-
-		maxZ = *std::max_element(m_meshPixels[i].zCoords.begin(), m_meshPixels[i].zCoords.end());
-
-		m_metrics[0][i] = maxZ;
-	}
-
-	exportMetrics("bielyKriz_namedBand_export.tif");
+	exit(0);
 }
 
 DataHandler::~DataHandler()
@@ -96,6 +43,9 @@ void DataHandler::readInputFile(QTextStream& stream)
 	m_areaInfo.width  = parsed[0].toInt();
 	m_areaInfo.height = parsed[1].toInt();
 
+	// compute desired real-world pixel size [m]
+	m_areaInfo.desiredPixelSize = m_areaInfo.width_n / m_areaInfo.width;
+
 	// read coordinates for upper left corner of mesh
 	temp = stream.readLine();
 	parsed = temp.split(";");
@@ -120,6 +70,9 @@ bool DataHandler::readLazFiles()
 	std::string fileName = "";
 	int nPointsInMesh = 0;
 
+	int nPixels = m_areaInfo.width_n * m_areaInfo.height_n;
+	m_meshPixels.resize(nPixels, PixelPoints());
+
 	auto start = std::chrono::high_resolution_clock::now();
 
 	for (int laz = 0; laz < m_areaInfo.lazFiles.size(); laz++)
@@ -133,7 +86,7 @@ bool DataHandler::readLazFiles()
 		}
 
 		fileName = lasReadOpener.get_file_name();
-		std::cout << "reading file: " << fileName << "\n";
+		std::cout << "reading file (" << laz + 1 << "/" << m_areaInfo.lazFiles.size() << "): " << fileName << "\n";
 
 		reader = lasReadOpener.open();
 		if (reader == nullptr)
@@ -183,10 +136,21 @@ bool DataHandler::readLazFiles()
 			// save point to the corresponding mesh pixel
 			pixelIdx = i * m_areaInfo.width_n + j;
 
-			m_meshPixels[pixelIdx].xCoords.push_back(ptX);
-			m_meshPixels[pixelIdx].yCoords.push_back(ptY);
-			m_meshPixels[pixelIdx].zCoords.push_back(ptZ);
-			m_meshPixels[pixelIdx].ptClass.push_back(ptClass);
+			if (ptClass == GROUND)
+			{
+				m_meshPixels[pixelIdx].groundPts.xCoords.push_back(ptX);
+				m_meshPixels[pixelIdx].groundPts.yCoords.push_back(ptY);
+				m_meshPixels[pixelIdx].groundPts.zCoords.push_back(ptZ);
+
+				nPointsInMesh++;
+
+				continue;
+			}
+
+			m_meshPixels[pixelIdx].vegetationPts.xCoords.push_back(ptX);
+			m_meshPixels[pixelIdx].vegetationPts.yCoords.push_back(ptY);
+			m_meshPixels[pixelIdx].vegetationPts.zCoords.push_back(ptZ);
+			m_meshPixels[pixelIdx].vegetationPts.ptClass.push_back(ptClass);
 
 			nPointsInMesh++;
 		}
@@ -208,19 +172,42 @@ bool DataHandler::readLazFiles()
 void DataHandler::normalizePoints()
 {
 	double minZ = -1.0;
+	double minV = -1.0;
+	double minG = -1.0;
+
+	bool noGroundPts = true;
+	bool noVegetationPts = true;
 
 	auto start = std::chrono::high_resolution_clock::now();
 
-#pragma omp parallel for private(minZ)
+#pragma omp parallel for private(minZ, minV, minG, noGroundPts, noVegetationPts)
 	for (int i = 0; i < m_meshPixels.size(); i++)
 	{
-		//std::cout << "pixel: " << i << "\n";
-		if (m_meshPixels[i].zCoords.empty())
+		// Check if there are points in pixel
+		noGroundPts = m_meshPixels[i].groundPts.zCoords.empty();
+		noVegetationPts = m_meshPixels[i].vegetationPts.zCoords.empty();
+		
+		if (noGroundPts && noVegetationPts)
 			continue;
 
-		minZ = *std::min_element(m_meshPixels[i].zCoords.begin(), m_meshPixels[i].zCoords.end());
+		if (noGroundPts)
+			minG = DBL_MAX;
+		else
+			minG = *std::min_element(m_meshPixels[i].groundPts.zCoords.begin(), m_meshPixels[i].groundPts.zCoords.end());
 
-		for (auto& z : m_meshPixels[i].zCoords)
+		if (noVegetationPts)
+			minV = DBL_MAX;
+		else
+			minV = *std::min_element(m_meshPixels[i].vegetationPts.zCoords.begin(), m_meshPixels[i].vegetationPts.zCoords.end());
+
+		minZ = (minG < minV) ? minG : minV;
+
+		for (auto& z : m_meshPixels[i].vegetationPts.zCoords)
+		{
+			z -= minZ;
+		}
+
+		for (auto& z : m_meshPixels[i].groundPts.zCoords)
 		{
 			z -= minZ;
 		}
@@ -233,18 +220,167 @@ void DataHandler::normalizePoints()
 
 }
 
+void DataHandler::redistributePoints()
+{
+	int nPixels = m_areaInfo.width * m_areaInfo.height;
+	m_meshPixelsRedistributed.resize(nPixels, PixelPoints());
+
+	int desiredPixelSize = m_areaInfo.desiredPixelSize;
+	int I = -1, J = -1;
+	int index = -1;
+	int INDEX = -1;
+	int j = -1, k = -1, l = -1;
+
+	std::vector<double>* v1 = nullptr;
+	std::vector<double>* v2 = nullptr;
+
+	std::vector<int>* v1i = nullptr;
+	std::vector<int>* v2i = nullptr;
+
+	auto start = std::chrono::high_resolution_clock::now();
+
+	// iterate over computation mesh with desired pixel size -> (i,j)
+#pragma omp parallel for private(j,k,l,I,J,v1,v2,v1i,v2i,index,INDEX)
+	for (int i = 0; i < m_areaInfo.height; i++)
+	{
+		for (j = 0; j < m_areaInfo.width; j++)
+		{
+			// iterate over normalization mesh -> (I,J): normalization mesh, (k,l): mask
+			for (k = 0; k < desiredPixelSize; k++)
+			{
+				for (l = 0; l < desiredPixelSize; l++)
+				{
+					// compute pixel indices in normalization mesh
+					I = i * desiredPixelSize + k;
+					J = j * desiredPixelSize + l;
+
+					if ((I < m_areaInfo.height_n) && (J < m_areaInfo.width_n)) // check if inside normalization mesh
+					{
+						INDEX = I * m_areaInfo.width_n + J; // compute correct INDEX in normalization mesh
+						index = i * m_areaInfo.width   + j; // compute correct index in desired computational mesh
+						
+						// move vegetation points
+						v1 = &m_meshPixelsRedistributed[index].vegetationPts.xCoords;
+						v2 = &m_meshPixels[INDEX].vegetationPts.xCoords;
+						v1->insert(v1->end(), v2->begin(), v2->end()); // insert v2 to v1
+						v2->clear(); v2->shrink_to_fit(); // clear v2 contents
+
+						v1 = &m_meshPixelsRedistributed[index].vegetationPts.yCoords;
+						v2 = &m_meshPixels[INDEX].vegetationPts.yCoords;
+						v1->insert(v1->end(), v2->begin(), v2->end()); // insert v2 to v1
+						v2->clear(); v2->shrink_to_fit(); // clear v2 contents
+
+						v1 = &m_meshPixelsRedistributed[index].vegetationPts.zCoords;
+						v2 = &m_meshPixels[INDEX].vegetationPts.zCoords;
+						v1->insert(v1->end(), v2->begin(), v2->end()); // insert v2 to v1
+						v2->clear(); v2->shrink_to_fit(); // clear v2 contents
+
+						v1i = &m_meshPixelsRedistributed[index].vegetationPts.ptClass;
+						v2i = &m_meshPixels[INDEX].vegetationPts.ptClass;
+						v1i->insert(v1i->end(), v2i->begin(), v2i->end()); // insert v2 to v1
+						v2i->clear(); v2i->shrink_to_fit(); // clear v2 contents
+
+						// move ground points
+						v1 = &m_meshPixelsRedistributed[index].groundPts.xCoords;
+						v2 = &m_meshPixels[INDEX].groundPts.xCoords;
+						v1->insert(v1->end(), v2->begin(), v2->end()); // insert v2 to v1
+						v2->clear(); v2->shrink_to_fit(); // clear v2 contents
+
+						v1 = &m_meshPixelsRedistributed[index].groundPts.yCoords;
+						v2 = &m_meshPixels[INDEX].groundPts.yCoords; // insert v2 to v1
+						v1->insert(v1->end(), v2->begin(), v2->end()); // clear v2 contents
+						v2->clear(); v2->shrink_to_fit(); // clear v2 contents
+
+						v1 = &m_meshPixelsRedistributed[index].groundPts.zCoords;
+						v2 = &m_meshPixels[INDEX].groundPts.zCoords; // insert v2 to v1
+						v1->insert(v1->end(), v2->begin(), v2->end()); // clear v2 contents
+						v2->clear(); v2->shrink_to_fit(); // clear v2 contents
+
+					}
+				}
+			} // END of mask iteration
+
+		} // proceed to new pixel
+	}
+
+	m_meshPixels.clear();
+	m_meshPixels.shrink_to_fit();
+
+	auto end = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+	printf("\nPoints redistribution: %.4lf s\n", (double)duration.count() / 1000.0);
+}
+
 void DataHandler::computeMetrics()
 {
+	int nPixels = m_areaInfo.width * m_areaInfo.height;
+	std::cout << "nPixels:" << nPixels << "\n";
 
+	auto start = std::chrono::high_resolution_clock::now();
+
+	m_metrics.resize(1);
+	for (int i = 0; i < m_metrics.size(); i++)
+	{
+		m_metrics[i] = new double[nPixels] {};
+	}
+
+	double maxZ = -1.0;
+	int nAllPts = -1;
+	int nGroundPts = -1;
+	int nVegetationPts = -1;
+	std::vector<double>* zValues = nullptr;
+
+#pragma omp parallel for private(zValues, nGroundPts, nVegetationPts, nAllPts, maxZ)
+	for (int i = 0; i < nPixels; i++)
+	{
+		m_metrics[0][i] = NAN;
+
+		zValues = &m_meshPixelsRedistributed[i].vegetationPts.zCoords;
+
+		nGroundPts = m_meshPixelsRedistributed[i].groundPts.zCoords.size();
+		nVegetationPts = zValues->size();
+		nAllPts = nGroundPts + nVegetationPts;
+
+
+		if (nAllPts == 0)
+			continue;
+
+		// PPR
+		//m_metrics[1][i] = static_cast<double>(nGroundPts) / nAllPts;
+
+		if (nVegetationPts == 0)
+			maxZ = 0.0;
+		else
+			maxZ = *std::max_element(zValues->begin(), zValues->end());
+
+		
+		// Hmax
+		m_metrics[0][i] = maxZ;
+	}
+
+	auto end = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+	printf("\nCompute metrics: %.4lf s\n", (double)duration.count() / 1000.0);
 }
 
 void DataHandler::exportMetrics(std::string fileName)
 {
+	time_t now = time(0);
+	tm* ltm = localtime(&now);
+	
+	std::string timeStamp = QString("%1_%2_%3_%4-%5-%6_").arg(ltm->tm_year + 1900,4).arg(ltm->tm_mon,2, 10, QLatin1Char('0')).arg(ltm->tm_mday, 2, 10, QLatin1Char('0')).arg(ltm->tm_hour, 2, 10, QLatin1Char('0')).arg(ltm->tm_min, 2, 10, QLatin1Char('0')).arg(ltm->tm_sec, 2, 10, QLatin1Char('0')).toStdString();
+	timeStamp.append(fileName);
+	fileName = timeStamp;
+	fileName.append(QString("_h=%1m").arg(m_areaInfo.desiredPixelSize).toStdString());
+	
 	if (fileName.substr(fileName.length() - 4, 4) != ".tif")
 	{
 		fileName.append(".tif");
 	}
-
+	
+	std::cout << "Exporting metrics...";
 	// load drivers
 	GDALAllRegister();
 	GDALDriver* poDriver;
@@ -252,10 +388,15 @@ void DataHandler::exportMetrics(std::string fileName)
 
 	GDALDataset* poDstDS = nullptr;
 	char** papszOptions = NULL;
-	poDstDS = poDriver->Create(fileName.c_str(), m_areaInfo.width_n, m_areaInfo.height_n, 1, GDT_Float64, papszOptions);
+	poDstDS = poDriver->Create(fileName.c_str(), m_areaInfo.width, m_areaInfo.height, m_metrics.size(), GDT_Float64, papszOptions);
 
 	// geotransform -> x,y lavy horny vrchol, dlzky v metroch pre pixely, rotacie (tie su 0)
-	double adfGeoTransform[6] = { m_areaInfo.xLeft, 1, 0, m_areaInfo.yLeft, 0, -1 };
+	double adfGeoTransform[6] = { m_areaInfo.xLeft,
+								  m_areaInfo.desiredPixelSize,
+		                          0,
+		                          m_areaInfo.yLeft,
+		                          0,
+		                         -m_areaInfo.desiredPixelSize };
 	poDstDS->SetGeoTransform(adfGeoTransform);
 
 	// Spatial reference system for tif
@@ -268,11 +409,60 @@ void DataHandler::exportMetrics(std::string fileName)
 
 	// Export bands
 	GDALRasterBand* poBand;
-	poBand = poDstDS->GetRasterBand(1);
-	poBand->SetDescription("1_Hmax");
-	poBand->RasterIO(GF_Write, 0, 0, m_areaInfo.width_n, m_areaInfo.height_n,
-		m_metrics[0], m_areaInfo.width_n, m_areaInfo.height_n, GDT_Float64, 0, 0);
+
+	for (int i = 0; i < m_metrics.size(); i++)
+	{
+		poBand = poDstDS->GetRasterBand(i + 1);
+		poBand->SetDescription(m_bandNames[i].c_str());
+		poBand->RasterIO(GF_Write, 0, 0, m_areaInfo.width, m_areaInfo.height,
+			m_metrics[i], m_areaInfo.width, m_areaInfo.height, GDT_Float64, 0, 0);
+	}
 	
 	// Close file
 	GDALClose((GDALDatasetH)poDstDS);
+
+	std::cout << " done\n";
+}
+
+void DataHandler::performCalculation(QString infoFile)
+{
+	QFile inputFile = QFile(infoFile);
+	if (!inputFile.open(QIODevice::ReadOnly))
+	{
+		std::cout << "Info file failed to open\n";
+		exit(-1);
+	}
+	
+	QTextStream inStream(&inputFile);
+
+	QString temp = inputFile.fileName().mid(inputFile.fileName().lastIndexOf("/") + 1);
+	m_areaName = temp.left(temp.length() - 4).toStdString();
+
+	readInputFile(inStream);
+
+	inputFile.close();
+
+	std::cout << "width_n: " << m_areaInfo.width_n << "\nheight_n: " << m_areaInfo.height_n << "\n";
+	std::cout << "width: " << m_areaInfo.width << "\nheight: " << m_areaInfo.height << "\n";
+	std::cout << "(x,y): [" << m_areaInfo.xLeft << ", " << m_areaInfo.yLeft << "]\n";
+	std::cout << "laz files: " << m_areaInfo.lazFiles.size() << "\n";
+
+	/*for (size_t i = 0; i < m_areaInfo.lazFiles.size(); i++)
+	{
+		std::cout << "file " << i << ": " << m_areaInfo.lazFiles[i] << "\n";
+	}*/
+
+	if (!readLazFiles())
+	{
+		std::cout << "failed to read laz files\n";
+		exit(-2);
+	}
+
+	normalizePoints();
+
+	redistributePoints();
+
+	computeMetrics();
+
+	exportMetrics(m_areaName);
 }
--- src/diplomovka_TH.h
+++ src/diplomovka_TH.h
@@ -30,12 +30,38 @@
 #include <cmath>
 #include <algorithm>
 #include <random>
+#include <ctime>
+
+#include "stat_functions.hpp"
 
 #define GROUND 2
 #define VEG_LOW 3
 #define VEG_MEDIUM 4
 #define VEG_HIGH 5
 
+#define Hmax 0
+#define Hmean 1
+#define Hmedian 2
+#define Hp25 3
+#define Hp75 4
+#define Hp95 5
+#define PPR 6
+#define DAM_z 7
+#define BR_bellow_1 8
+#define BR_1_2 9
+#define BR_2_3 10
+#define BR_above_3 11
+#define BR_3_4 12
+#define BR_4_5 13
+#define BR_bellow_5 14
+#define BR_5_20 15
+#define BR_above_20 16
+#define Coeff_var_z 17
+#define Hkurt 18
+#define Hskew 19
+#define Hstd 20
+#define Hvar 21
+#define Shannon 22
 
 struct AreaInfo
 {
@@ -43,17 +69,31 @@ struct AreaInfo
 	int height_n = -1;
 	int width = -1;
 	int height = -1;
+	int desiredPixelSize = -1;
 	double xLeft = -1.0;
 	double yLeft = -1.0;
 	std::vector<std::string> lazFiles;
 };
 
-struct Points
+struct VegetationPoints
 {
 	std::vector<double> xCoords;
 	std::vector<double> yCoords;
 	std::vector<double> zCoords;
 	std::vector<int> ptClass;
+};
+
+struct GroundPoints
+{
+	std::vector<double> xCoords;
+	std::vector<double> yCoords;
+	std::vector<double> zCoords;
+};
+
+struct PixelPoints
+{
+	VegetationPoints vegetationPts;
+	GroundPoints groundPts;
 };
 
 class DataHandler : public QMainWindow
@@ -68,8 +108,12 @@ private:
 	Ui::NaturaSat_TSPClass ui;
 
 	AreaInfo m_areaInfo;
-	std::vector<Points> m_meshPixels;
+	std::vector<PixelPoints> m_meshPixels;
+	std::vector<PixelPoints> m_meshPixelsRedistributed;
 	std::vector<double*> m_metrics;
+	std::string m_areaName = "";
+
+	std::vector<std::string> m_bandNames = { "Hmax","Hmean","Hmedian","Hp25","Hp75","Hp95","PPR","DAM_z","BR_bellow_1","BR_1_2","BR_2_3","BR_above_3","BR_3_4","BR_4_5","BR_bellow_5","BR_5_20","BR_above_20","Coeff_var_z","Hkurt","Hskew","Hstd","Hvar","Shannon" };
 
 	void readInputFile(QTextStream& stream);
 
@@ -77,7 +121,11 @@ private:
 
 	void normalizePoints();
 
+	void redistributePoints();
+
 	void computeMetrics();
 
 	void exportMetrics(std::string fileName);
+
+	void performCalculation(QString infoFile);
 };
